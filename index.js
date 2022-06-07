1497e5a7f91cf7d17e43f979f110203d24054cf9
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-06-07 12:13:39 -0300

    feat: creates routes users and urls

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
 
+import usersRouter from "./routes/usersRouter.js";
+import urlsRouter from "./routes/urlsRouter.js";
+
 const app = express();
 dotenv.config()
 
 app.use(json());
-app.use(cors());    
+app.use(cors());
+
+app.use(usersRouter);
+app.use(urlsRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(chalk.white.bold.bgGreenBright(`\n Application is running on port ${port}... \n`)));
