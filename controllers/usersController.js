c892f8d6a66db003a82093d8c6449b10e38195b9
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-06-07 12:50:59 -0300

    feat: signs up user

---
+++ controllers/usersController.js
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+
+import db from "../database/db.js";
+
+export async function signUpUser(req, res) {
+    const user = req.body;
+    const values = Object.values(user).pop();
+
+    try {
+        await db.query("INSERT INTO users(name, email, password) VALUES ($1, $2, $3)", values);
+        return res.sendStatus(201);
+    } catch (e) {
+        console.log(chalk.red.bold("\nAn error occured while trying to sign up user."));
+        return res.status(500).send(e);
+    }
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { signUpUser } from "../controllers/usersController.js";
+
 const usersRouter = Router();
+
+usersRouter.post("/signup", signUpUser);
 
 export default usersRouter;
